27780863e380ffee9b33469e6e35cc0b27d4c05d
1KrishnaSingh346 <145567074+1KrishnaSingh346@users.noreply.github.com>
2025-04-12 23:09:46 +0530

    Changes in index.js

--- src/index.js
+++ src/index.js
@@ -33,10 +33,10 @@ app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === "production")
 {
-  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // when system go under production
+  app.use(express.static(path.join(__dirname, "../Frontend/dist"))); // when system go under production
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
